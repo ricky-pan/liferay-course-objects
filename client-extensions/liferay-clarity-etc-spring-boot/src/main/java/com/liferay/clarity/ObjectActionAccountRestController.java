51081aa62311899b9e2659351fe8933f048ec205
Ricky Pan <ricky.pan@liferay.com>
2024-12-09 16:06:21 -0800

    Update client extension to point to correct object attribute

--- client-extensions/liferay-clarity-etc-spring-boot/src/main/java/com/liferay/clarity/ObjectActionAccountRestController.java
+++ client-extensions/liferay-clarity-etc-spring-boot/src/main/java/com/liferay/clarity/ObjectActionAccountRestController.java
@@ -44,11 +44,11 @@ public class ObjectActionAccountRestController extends BaseRestController {
 
 		JSONObject jsonObject = new JSONObject(json);
 
-		JSONObject objectEntryDTOD4B8DistributorApplicationJSONObject =
-			jsonObject.getJSONObject("objectEntryDTOD4B8DistributorApplication");
+		JSONObject objectEntryDTODistributorApplicationJSONObject =
+			jsonObject.getJSONObject("objectEntryDTODistributorApplication");
 
 		JSONObject propertiesJSONObject =
-			objectEntryDTOD4B8DistributorApplicationJSONObject.getJSONObject(
+			objectEntryDTODistributorApplicationJSONObject.getJSONObject(
 				"properties");
 
 		String accountEmailAddress = propertiesJSONObject.getString(
